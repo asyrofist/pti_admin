329ea97260369597554fde34c6bdee99eff35f21
Rakha Asyrofi <asyrofi@hangtuah.ac.id>
2022-06-29 20:01:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, surat_keputusan, surat_tugas, petikan_keputusan,invoice, email_sent  # import your app modules here
-from streamlit_option_menu import option_menu
+from apps import home, surat_keputusan, surat_tugas, petikan_keputusan,invoice # import your app modules here
 
 
 st.set_page_config(layout="centered", page_icon="img/hangtuah_icon.jpg", page_title="Surat Keputusan Generator")
@@ -14,8 +13,6 @@ app.add_app("Surat Keputusan", surat_keputusan.app)
 app.add_app("Surat Tugas", surat_tugas.app)
 app.add_app("Petikan Keputusan", petikan_keputusan.app)
 app.add_app("Invoice", invoice.app)
-app.add_app("email_sSnd", email_sent.app)
-
 
 # The main app
 app.run()
