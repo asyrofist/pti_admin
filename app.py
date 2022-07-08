bfe0bdec3472babf23bfca3005847a9ff544375d
papasyrofi <pap.rakhasyrofi@gmail.com>
2022-07-08 14:02:49 +0700

    update crud

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, surat_keputusan, surat_tugas, petikan_keputusan,invoice, email_python, email_yag  # import your app modules here
+from apps import home, surat_keputusan, surat_tugas, petikan_keputusan,invoice, email_python, email_yag, crud  # import your app modules here
 
 st.set_page_config(layout="centered", page_icon="img/hangtuah_icon.jpg", page_title="Surat Keputusan Generator")
 
@@ -14,6 +14,7 @@ app.add_app("Petikan Keputusan", petikan_keputusan.app)
 app.add_app("Invoice", invoice.app)
 app.add_app("email_python", email_python.app)
 app.add_app("email_yag", email_yag.app)
+app.add_app("CRUD", crud.app)
 
 # The main app
 app.run()
